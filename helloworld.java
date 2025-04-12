ce08458f4ba76ae2292b567fa3d67eaecdebafb5
phanindra4568 <137140900+phanindra4568@users.noreply.github.com>
2025-04-12 21:42:20 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello, World! The container will stay alive for 10 minutes.");
+        try {
+            Thread.sleep(10 * 60 * 1000); // Sleep for 10 minutes (600,000 ms)
+        } catch (InterruptedException e) {
+            System.out.println("Sleep interrupted.");
+        }
+    }
 }
